7e32dc35cb07d02c1db174d5303707c0fa62559a
Fer Hernandez <fh.1111@gmail.com>
2018-12-29 21:50:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,4 +6,13 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Fer"
   end
+
+  get '/hometown' do
+    "My hometown is SC"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Guerras Perdidas"
+  end
+  
 end
